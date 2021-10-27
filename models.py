ec13c85dd12b1babf4d3fe1fd13bf818724205f5
Horatiu-Catalin Pop <PsyHo.3D@gmail.com>
2021-10-27 18:13:03 +0300

    DBWithUser

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, Text, Enum, ForeignKey, Float, Table,VAR
 
 from dbSessionCreator import Base
 
-Table.schema='testschema'
+Table.schema='winecheese'
 
 
 class IntensityTypes(enum.Enum):
@@ -27,63 +27,63 @@ class WineColors(enum.Enum):
 
 class WineCollor(Base):
     __tablename__ = 'WineColor'
-    __table_args__ = {"schema": "testschema"}
+    __table_args__ = {"schema": "winecheese"}
     id_wine_collor = Column(Integer, primary_key=True)
     color = Column(Enum(WineColors))
 
 
 class Intensity(Base):
     __tablename__ = 'WineIntensity'
-    __table_args__ = {"schema": "testschema"}
+    __table_args__ = {"schema": "winecheese"}
     id_wine_intensity = Column(Integer, primary_key=True)
     intensity = Column(Enum(IntensityTypes))
 
 
-class Body(Base):
-    __tablename__ = 'Body'
-    __table_args__ = {"schema": "testschema"}
+class WineBody(Base):
+    __tablename__ = 'WineBody'
+    __table_args__ = {"schema": "winecheese"}
     id_wine_body = Column(Integer, primary_key=True)
     body = Column(Enum(BodyTypes))
 
 
-class WineType(Base):
-    __tablename__ = 'WineType'
-    __table_args__ = {"schema": "testschema"}
-    id_wine_type = Column(Integer, primary_key=True)
-    type_of_wine = Column(VARCHAR(20))
+class WineVariety(Base):
+    __tablename__ = 'WineVariety'
+    __table_args__ = {"schema": "winecheese"}
+    id_wine_variety = Column(Integer, primary_key=True)
+    variety_of_wine = Column(VARCHAR(20))
 
 
 class Wine(Base):
     __tablename__ = 'Wine'
-    __table_args__ = {"schema": "testschema"}
+    __table_args__ = {"schema": "winecheese"}
     id_w = Column(Integer, primary_key=True)
-    color = Column(Integer, ForeignKey('testschema.WineColor.id_wine_collor'))
-    intensity = Column(Integer, ForeignKey('testschema.WineIntensity.id_wine_intensity'))
+    color = Column(Integer, ForeignKey('winecheese.WineColor.id_wine_collor'))
+    intensity = Column(Integer, ForeignKey('winecheese.WineIntensity.id_wine_intensity'))
     pH = Column(Float)
     sugar = Column(Float)
     acidity = Column(Float)
     alcohol = Column(Float)
     tannins = Column(Float)
-    body = Column(Integer, ForeignKey('testschema.Body.id_wine_body'))
-    variety = Column(Integer,ForeignKey('testschema.WineType.id_wine_type'))
+    body = Column(Integer, ForeignKey('winecheese.WineBody.id_wine_body'))
+    variety = Column(Integer,ForeignKey('winecheese.WineVariety.id_wine_variety'))
 
 
 class Cheese(Base):
     __tablename__ = 'Cheese'
-    __table_args__ = {"schema": "testschema"}
+    __table_args__ = {"schema": "winecheese"}
     id_c = Column(Integer, primary_key=True)
     cheese=Column(VARCHAR(20))
 
 
 Pairing = Table('Pairing',Base.metadata,
-                Column('id_wine', ForeignKey('testschema.Wine.id_w')),
-                Column('id_cheese', ForeignKey('testschema.Cheese.id_c')),
-                schema='testschema'
+                Column('id_wine', ForeignKey('winecheese.Wine.id_w')),
+                Column('id_cheese', ForeignKey('winecheese.Cheese.id_c')),
+                schema='winecheese'
                 )
 
 class User(Base):
     __tablename__='User'
-    __table_args__ = {"schema": "testschema"}
+    __table_args__ = {"schema": "winecheese"}
     id_user=Column(Integer,primary_key=True)
     username=Column(VARCHAR(20))
     password=Column(VARCHAR(20))#we must implement hashing
