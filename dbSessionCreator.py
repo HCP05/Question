6f20868b4c911bfe1081a50d8f51b0fec44dd5e5
Horatiu-Catalin Pop <PsyHo.3D@gmail.com>
2021-10-27 15:06:09 +0300

    Finalized

--- dbSessionCreator.py
+++ dbSessionCreator.py
@@ -13,8 +13,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import pyodbc
 
-SQL_URL = 'mssql+pyodbc://richard_koch:0ptZeciD0u@z3c1@python-internship-server.database.windows.net/python_internship_sample?driver=SQL Server'
-# SQL_URL = 'mssql+pyodbc://@HCP05\SQLEXPRESS/test'
+# SQL_URL = 'mssql+pyodbc://richard_koch:0ptZeciD0u@z3c1@python-internship-server.database.windows.net/python_internship_sample?driver=SQL Server'
+SQL_URL = 'mssql+pyodbc://@HCP05\SQLEXPRESS/test?driver=SQL Server'
 
 engine = create_engine(
     SQL_URL
--- models.py
+++ models.py
@@ -4,23 +4,22 @@ from sqlalchemy import Column, Integer, Text, Enum, ForeignKey, Float, Table
 
 from dbSessionCreator import Base
 
+Table.schema='testschema'
+
 
 class IntensityTypes(enum.Enum):
-    __table_args__ = {"schema": "test"}
     light = 1
     medium = 2
     rich = 3
 
 
 class BodyTypes(enum.Enum):
-    __table_args__ = {"schema": "test"}
     light = 1
     medium = 2
     full = 3
 
 
 class WineColors(enum.Enum):
-    __table_args__ = {"schema": "test"}
     red = 1
     white = 2
     rose = 3
@@ -28,56 +27,57 @@ class WineColors(enum.Enum):
 
 class WineCollor(Base):
     __tablename__ = 'WineColor'
-    __table_args__ = {"schema": "test"}
+    __table_args__ = {"schema": "testschema"}
     id_wine_collor = Column(Integer, primary_key=True)
     color = Column(Enum(WineColors))
 
 
 class Intensity(Base):
     __tablename__ = 'WineIntensity'
-    __table_args__ = {"schema": "test"}
+    __table_args__ = {"schema": "testschema"}
     id_wine_intensity = Column(Integer, primary_key=True)
     intensity = Column(Enum(IntensityTypes))
 
 
 class Body(Base):
     __tablename__ = 'Body'
-    __table_args__ = {"schema": "test"}
+    __table_args__ = {"schema": "testschema"}
     id_wine_body = Column(Integer, primary_key=True)
     body = Column(Enum(BodyTypes))
 
 
 class WineType(Base):
     __tablename__ = 'WineType'
-    __table_args__ = {"schema": "test"}
+    __table_args__ = {"schema": "testschema"}
     id_wine_type = Column(Integer, primary_key=True)
     type_of_wine = Column(Text)
 
 
 class Wine(Base):
     __tablename__ = 'Wine'
-    __table_args__ = {"schema": "test"}
+    __table_args__ = {"schema": "testschema"}
     id_w = Column(Integer, primary_key=True)
-    color = Column(Integer, ForeignKey('test.WineColor.id_wine_collor'))
-    intensity = Column(Integer, ForeignKey('test.WineIntensity.id_wine_intensity'))
+    color = Column(Integer, ForeignKey('testschema.WineColor.id_wine_collor'))
+    intensity = Column(Integer, ForeignKey('testschema.WineIntensity.id_wine_intensity'))
     pH = Column(Float)
     sugar = Column(Float)
     acidity = Column(Float)
     alcohol = Column(Float)
     tannins = Column(Float)
-    body = Column(Integer, ForeignKey('test.Body.id_wine_body'))
-    variety = Column(Integer,ForeignKey('test.WineType.id_wine_type'))
+    body = Column(Integer, ForeignKey('testschema.Body.id_wine_body'))
+    variety = Column(Integer,ForeignKey('testschema.WineType.id_wine_type'))
 
 
 class Cheese(Base):
     __tablename__ = 'Cheese'
-    __table_args__ = {"schema": "test"}
+    __table_args__ = {"schema": "testschema"}
     id_c = Column(Integer, primary_key=True)
     cheese=Column(Text)
 
 
-Pairing = Table('Pairing',
-                Column('id_wine', ForeignKey('test.Wine.id_w')),
-                Column('id_cheese', ForeignKey('test.Cheese.id_c')),
-                schema="python_internship_sample.test"
+Pairing = Table('Pairing',Base.metadata,
+                Column('id_wine', ForeignKey('testschema.Wine.id_w')),
+                Column('id_cheese', ForeignKey('testschema.Cheese.id_c')),
+                schema='testschema'
                 )
+
